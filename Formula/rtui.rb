b8f38bdbbf0ac3247edb68d23efa6364d2f5fd54
mrekoj <hoangnn@gmail.com>
2026-02-03 13:58:56 +0700

    Bump rtui to v0.1.2

--- Formula/rtui.rb
+++ Formula/rtui.rb
@@ -1,14 +1,14 @@
 class Rtui < Formula
   desc "Minimal TUI dashboard to monitor and manage multiple git repos"
   homepage "https://github.com/mrekoj/rtui"
-  version "0.1.1"
+  version "0.1.2"
 
   if Hardware::CPU.arm?
-    url "https://github.com/mrekoj/rtui/releases/download/v0.1.1/rtui_darwin_arm64.tar.gz"
-    sha256 "526a52addcbbabbe0f9168bad8b9039034fc763649dbb4f052ecef0ea9aedb5e"
+    url "https://github.com/mrekoj/rtui/releases/download/v0.1.2/rtui_darwin_arm64.tar.gz"
+    sha256 "fe072a964210b3f28f9e1fa99c25e56f533ee0f809b374f471ad1e529db24cd0"
   else
-    url "https://github.com/mrekoj/rtui/releases/download/v0.1.1/rtui_darwin_amd64.tar.gz"
-    sha256 "1cb31d4787a36748b766a325047b428d146f22442d62259f6ab3e161aea1e1fb"
+    url "https://github.com/mrekoj/rtui/releases/download/v0.1.2/rtui_darwin_amd64.tar.gz"
+    sha256 "985310eef69a3c347dd30ad34604960496b58ad390fdd49b62e8a7d1e7d6244b"
   end
 
   def install
